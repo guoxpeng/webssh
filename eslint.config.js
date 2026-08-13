48998d196f1ccc7dd2caa702d899a94f15890cce
guoxpeng <guoxpeng@users.noreply.github.com>
2026-08-13 12:13:29 +0800

    fix: lint breaks on brace-expansion override and vue SFC parsing

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import pluginVue from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import prettier from 'eslint-config-prettier';
@@ -8,14 +9,35 @@ export default [
     ignores: ['dist/**', 'node_modules/**'],
   },
   {
-    files: ['web/src/**/*.{js,ts,vue}'],
+    files: ['web/src/**/*.{js,ts}'],
     languageOptions: {
       parser: tsParser,
       parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
     },
     plugins: {
       '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      ...prettier.rules,
+    },
+  },
+  {
+    files: ['web/src/**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        parser: tsParser,
+        extraFileExtensions: ['.vue'],
+      },
+    },
+    plugins: {
       vue: pluginVue,
+      '@typescript-eslint': tsPlugin,
     },
     rules: {
       'no-console': ['warn', { allow: ['warn', 'error'] }],
